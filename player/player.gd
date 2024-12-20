66c42f1dc9ba994fbe9e070aec35a784f7b7770b
MomoelBG <mohamed.sadek1605@gmail.com>
2024-12-20 19:42:47 +0100

    script

--- player/player.gd
+++ player/player.gd
@@ -68,7 +68,7 @@ func _physics_process(delta):
 		
 		
 		if Input.is_action_just_pressed("lclick"):
-			picked_object()
+			picked_object
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
