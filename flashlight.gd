4750aa77502d777dcb82053840208748ff2cfd31
MomoelBG <mohamed.sadek1605@gmail.com>
2024-12-21 09:27:15 +0100

    f

--- flashlight.gd
+++ flashlight.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("Toggle"):
+	if Input.is_action_pressed("Toggle"):
 		$SpotLight3D.light_energy = 32
 	else:
 		$SpotLight3D.light_energy = 0
