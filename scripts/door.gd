435918de58201ec14b6553b947698e53f5003fe1
MomoelBG <mohamed.sadek1605@gmail.com>
2024-12-21 14:15:37 +0100

    door

---
+++ scripts/door.gd
@@ -0,0 +1,16 @@
+extends StaticBody3D
+
+var toggle = false
+var interactable = true
+@export var anim: AnimationPlayer
+
+func interact():
+	if interactable == true:
+		interactable = false
+		toggle = !toggle
+		if toggle == false:
+			anim.play("close")
+		if toggle == true:
+			anim.play("open")
+		await get_tree().create_timer(1.0, false).timeout
+		interactable = true
