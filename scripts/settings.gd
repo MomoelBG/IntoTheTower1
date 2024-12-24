b0f96d36a793827db263af4e8a1e357044d9aad5
MomoelBG <mohamed.sadek1605@gmail.com>
2024-12-24 11:29:57 +0100

    InputMap

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var resolution = $resolution
 @onready var controles = $controles
 @onready var settings: Control = $"."
+@onready var input_settings = $InputSettings
 
 
 func _ready():
@@ -19,16 +20,25 @@ func _on_audio_pressed():
 	audio.visible = true
 	resolution.visible = false
 	controles.visible = false
+	input_settings.visible = false
 
 func _on_resolution_pressed():
 	audio.visible = false
 	controles.visible = false
 	resolution.visible = true
+	input_settings.visible = false
 
 func _on_controls_pressed():
 	audio.visible = false
 	resolution.visible = false
 	controles.visible = true
+	input_settings.visible = false
+
+func _on_input_controls_pressed():
+	input_settings.visible = true
+	audio.visible = false
+	resolution.visible = false
+	controles.visible = false
 
 func _on_h_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(0,value)
